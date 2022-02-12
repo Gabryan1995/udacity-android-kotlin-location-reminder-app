2443e6eb9da14e537f6d20082dfc951375a7a0b7
Garrett Bryan <gabryan1995@gmail.com>
2022-02-11 18:04:52 -0600

    Adding Koin modules
    Added module file to hold Koin modules for app.

---
+++ app/src/main/java/com/udacity/project4/utils/Modules.kt
@@ -0,0 +1,19 @@
+package com.udacity.project4.utils
+
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+
+    single<ReminderDataSource> { RemindersLocalRepository(get(), get()) }
+
+    //Declare a ViewModel - be later inject into Fragment with dedicated injector using by viewModel()
+    viewModel {
+        RemindersListViewModel(
+            get(),
+            get() as ReminderDataSource
+        )
+    }
+}
